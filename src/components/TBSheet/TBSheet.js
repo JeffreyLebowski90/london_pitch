aa41ee9dc5ee7ade6ab6b83dff56845091f5ac84
Marceddu <edoardo.marceddu@imi-critical.com>
2019-01-15 12:08:11 +0100

    aggiunto componente tableBody

---
+++ src/components/TBSheet/TBSheet.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const tBSheet = () => {
+  return (
+    <tbody>
+        <tr>
+            <th scope="row">#1</th>
+            <td>A5</td>
+            <td>100%</td>
+            <td>100 IMI₵</td>
+            <td>60s</td>
+            <td></td>
+        </tr>
+        <tr>
+            <th scope="row">#2</th>
+            <td>A4</td>
+            <td>100%</td>
+            <td>200 IMI₵</td>
+            <td>120s</td>
+            <td></td>
+        </tr>
+        <tr>
+            <th scope="row">#3</th>
+            <td>A3</td>
+            <td>100%</td>
+            <td>300 IMI₵</td>
+            <td>180s</td>
+            <td></td>
+        </tr>
+    </tbody>
+  )
+}
+
+export default tBSheet
--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,47 +1,29 @@
 import React from 'react'
+import TBSheet from '../TBSheet/TBSheet'
+
+// const sheets = [
+//     ["A5", "100%", "100 IMI₵", "60s", "" ],
+//     ["A4", "100%", "200 IMI₵", "120s", "" ],
+//     ["A3", "100%", "300 IMI₵", "180s", "" ]
+//   ]
+
 
 const table = () => {
-
-  return (
-    <table className="table table-dark">
-    <thead>
-        <tr>
-        <th scope="col">Number</th>
-        <th scope="col">Description</th>
-        <th scope="col">Matching</th>
-        <th scope="col">Price</th>
-        <th scope="col">Delivery</th>
-        <th scope="col">Select</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-        <th scope="row">#1</th>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-        <td>@mdo</td>
-        <td>@mdo</td>
-        </tr>
-        <tr>
-        <th scope="row">#2</th>
-        <td>Jacob</td>
-        <td>Thornton</td>
-        <td>@fat</td>
-        <td>@fat</td>
-        <td>@fat</td>
-        </tr>
-        <tr>
-        <th scope="row">#3</th>
-        <td>Larry</td>
-        <td>the Bird</td>
-        <td>@twitter</td>
-        <td>@twitter</td>
-        <td>@twitter</td>
-        </tr>
-    </tbody>
-    </table>
-  )
-}
+        return (
+            <table className="table table-dark">
+                <thead>
+                    <tr>
+                    <th scope="col">Number</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Matching</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Delivery</th>
+                    <th scope="col">Select</th>
+                    </tr>
+                </thead>
+                <TBSheet/>
+            </table>
+        )
+    }
 
 export default table
